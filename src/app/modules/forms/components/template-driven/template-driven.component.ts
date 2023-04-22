ab1195db706d2c5dfafa40be78dd4ddf583b2ee7
pavlo <pavlo.chaban@rocketharbor.com>
2023-04-22 14:24:48 +0300

    add to template drive form same functionality as for reactive form

--- src/app/modules/forms/components/template-driven/template-driven.component.ts
+++ src/app/modules/forms/components/template-driven/template-driven.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectionStrategy, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -10,6 +10,9 @@ import { CommonModule } from '@angular/common';
 import { WordsBlacklistDirective } from './validators/words-blacklist.validators';
 import { PasswordMatcherDirective } from './validators/password-matcher.validators';
 import { UniqueEmailDirective } from './async-validators/unique-email.async-validator';
+import { EMPTY, Observable, Subject, takeUntil } from 'rxjs';
+import { FormsDataService } from '@modules/forms/services/forms-data.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   templateUrl: 'template-driven.component.html',
@@ -24,22 +27,40 @@ import { UniqueEmailDirective } from './async-validators/unique-email.async-vali
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
+    MatCheckboxModule,
     WordsBlacklistDirective,
     PasswordMatcherDirective,
     UniqueEmailDirective,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TemplateDrivenFormComponent implements AfterViewInit {
+export class TemplateDrivenFormComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild(NgForm) form!: NgForm;
 
+  public phones: string[] = ['1'];
+  public skills$: Observable<string[]> = EMPTY;
+
   private _initialFormValues: unknown;
+  private _onDestroy$ = new Subject<void>();
+
+  constructor(
+    private _formsDataService: FormsDataService,
+  ) { }
+
+  public ngOnInit(): void {
+    this._loadSkills();
+  }
 
   public ngAfterViewInit(): void {
     setTimeout(() => {
       this._initialFormValues = this.form.value;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this._onDestroy$.next();
+    this._onDestroy$.complete();
   }
 
   public onSubmit(event: SubmitEvent): void {
@@ -52,6 +73,24 @@ export class TemplateDrivenFormComponent implements AfterViewInit {
   public onReset(event: Event): void {
     event.preventDefault();
     this.form.resetForm(this._initialFormValues);
+  }
+
+  public onAddPhone(event: MouseEvent): void {
+    event.stopPropagation();
+    const index = +(this.phones[this.phones.length - 1] || '0') + 1;
+    this.phones.push(index.toString());
+  }
+
+  public onDeletePhone(event: MouseEvent, index: number): void {
+    event.stopPropagation();
+    this.phones.splice(index, 1);
+  }
+
+  private _loadSkills(): void {
+    this.skills$ = this._formsDataService.getSkills()
+      .pipe(
+        takeUntil(this._onDestroy$)
+      );
   }
 
 }
