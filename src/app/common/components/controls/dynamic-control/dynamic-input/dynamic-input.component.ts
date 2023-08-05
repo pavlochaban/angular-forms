737bf9519c120f08521d785768259cb00d7412f2
pavlo <pavlo.chaban@rocketharbor.com>
2023-08-05 12:06:55 +0300

    implement dynamic input

---
+++ src/app/common/components/controls/dynamic-control/dynamic-input/dynamic-input.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { DynamicControlBaseDirective, dynamicControlProvider } from '../dynamic-control-base.component';
+import { InputFormControlComponent } from '../../input-form-control/input-form-control.component';
+import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { clone } from 'lodash';
+import { DynamicInputSuffixPipe } from './pipes/dynamic-input-suffix.pipe';
+import { DynamicInputLabelPipe } from './pipes/dynamic-input-label.pipe';
+
+@Component({
+  templateUrl: 'dynamic-input.component.html',
+  standalone: true,
+  viewProviders: [dynamicControlProvider],
+  imports: [
+    InputFormControlComponent,
+    ReactiveFormsModule,
+    DynamicInputSuffixPipe,
+    DynamicInputLabelPipe,
+  ],
+  host: {
+    class: 'dynamic-form-input form__field'
+  }
+})
+export class DynamicInputComponent extends DynamicControlBaseDirective {
+
+  public override abstractControl: FormControl = new FormControl(this.controlData.config.value, this.resolveValidators(this.controlData.config));
+
+  public onSuffixClick(): void {
+    const method = this.controlData.config.suffix?.clickMethod;
+    if (method) {
+      this[method](this.controlData.controlKey);
+    }
+  }
+
+  private deleteFromArray(key: string): void {
+    const formArray = this.controlContainer.control as FormArray;
+    const controlIndex = formArray.controls.findIndex(control => control === this.abstractControl);
+    formArray.removeAt(controlIndex)
+    const controls = clone(this.controlData.parentConfig?.controls);
+    delete controls![key];
+    this.controlData.parentConfig!.controls = controls;
+  }
+
+}
---
+++ src/app/common/components/controls/dynamic-control/dynamic-input/pipes/dynamic-input-label.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DynamicControl } from '../../dynamic-control.model';
+
+@Pipe({
+  name: 'dynamicInputLabel',
+  standalone: true,
+})
+export class DynamicInputLabelPipe implements PipeTransform {
+
+  transform(config: DynamicControl, parentConfig: DynamicControl | undefined): string {
+    if (!parentConfig) { return config.label; }
+
+    return config.label + ' #' + (config.order + 1)
+  }
+
+}
