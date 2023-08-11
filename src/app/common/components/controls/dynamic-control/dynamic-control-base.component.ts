b775a0cfd24df47489121e30c9cf1a9acb67c994
pavlo <pavlo.chaban@rocketharbor.com>
2023-08-11 17:41:48 +0300

    extend dynamic control base with method for resolving async validators

--- src/app/common/components/controls/dynamic-control/dynamic-control-base.component.ts
+++ src/app/common/components/controls/dynamic-control/dynamic-control-base.component.ts
@@ -1,7 +1,9 @@
 import { Directive, ElementRef, OnInit, Renderer2, StaticProvider, inject } from '@angular/core';
-import { AbstractControl, ControlContainer, FormArray, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, ControlContainer, FormArray, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { CONTROL_DATA } from './dynamic-control-data-token';
-import { DynamicControl, ValidatorKeys } from './dynamic-control.model';
+import { AsyncValidatorsKeys, DynamicControl, ValidatorKeys } from './dynamic-control.model';
+import { UniqueEmailValidationService } from '@modules/forms/validators/unique-email.async-validator';
+import { PasswordMatcher } from '@modules/forms/validators/password-matcher.validators';
 
 export const dynamicControlProvider: StaticProvider = {
   provide: ControlContainer,
@@ -16,6 +18,8 @@ export class DynamicControlBaseDirective implements OnInit {
   public controlData = inject(CONTROL_DATA);
   public controlContainer = inject(ControlContainer);
   public abstractControl!: AbstractControl;
+
+  private _uniqueEmailValidationService = inject(UniqueEmailValidationService, { optional: true });
 
   public ngOnInit(): void {
     this._addCustomClass();
@@ -36,16 +40,45 @@ export class DynamicControlBaseDirective implements OnInit {
     return validatorFns;
   }
 
+  protected resolveAsyncValidators({ asyncValidators = {} }: DynamicControl): AsyncValidatorFn[] {
+    const asyncValidatorKeys = (Object.keys(asyncValidators) as Array<keyof typeof asyncValidators>);
+    const asyncValidatorFns = <AsyncValidatorFn[]>asyncValidatorKeys.map(key => this._getAsyncValidatorByKey(key, asyncValidators)).filter(Boolean);
+
+    return asyncValidatorFns;
+  }
+
   private _getValidatorByKey(key: ValidatorKeys, validators: DynamicControl['validators'] = {}): ValidatorFn {
     const validatorParams = validators[key];
     if (key === 'required') {
       return Validators.required;
     }
+    if (key === 'email') {
+      return Validators.email;
+    }
     if (key === 'minLength' && typeof validatorParams === 'number') {
       return Validators.minLength(validatorParams);
     }
+    if (key === 'maxLength' && typeof validatorParams === 'number') {
+      return Validators.maxLength(validatorParams);
+    }
+    if (key === 'pattern' && (typeof validatorParams === 'string' || validatorParams instanceof RegExp)) {
+      return Validators.pattern(validatorParams);
+    }
+    if (key === 'passwordMatcher') {
+      return PasswordMatcher;
+    }
 
     return Validators.nullValidator;
+  }
+
+  private _getAsyncValidatorByKey(key: AsyncValidatorsKeys, validators: DynamicControl['asyncValidators'] = {}): AsyncValidatorFn | null {
+    const validatorParams = validators[key];
+
+    if (key === 'uniqueEmail' && !!this._uniqueEmailValidationService) {
+      return this._uniqueEmailValidationService.validate.bind(this._uniqueEmailValidationService);
+    }
+
+    return null;
   }
 
   private _addCustomClass(): void {
