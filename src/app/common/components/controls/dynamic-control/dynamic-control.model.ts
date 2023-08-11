5e531e41b1b2682749db21789b37bc757e5e0c38
pavlo <pavlo.chaban@rocketharbor.com>
2023-08-11 17:40:09 +0300

    extend dynamic control model with custom/async validators, updateOn and autocomplete props

--- src/app/common/components/controls/dynamic-control/dynamic-control.model.ts
+++ src/app/common/components/controls/dynamic-control/dynamic-control.model.ts
@@ -2,7 +2,9 @@ import { Validators } from "@angular/forms";
 
 export type DynamicControlType = 'input' | 'group' | 'array';
 
-export type ValidatorKeys = keyof Omit<typeof Validators, 'composeAsync' | 'compose' | 'prototype'>;
+export type AsyncValidatorsKeys = 'uniqueEmail';
+export type CustomValidatorKeys = 'passwordMatcher';
+export type ValidatorKeys = keyof Omit<typeof Validators, 'composeAsync' | 'compose' | 'prototype'> | CustomValidatorKeys;
 
 export interface DynamicControlSuffix {
   color: 'warn';
@@ -18,10 +20,15 @@ export interface DynamicControl<T = string> {
   order: number;
   value: T | null;
   htmlClass?: string;
+  updateOn?: 'change' | 'blur' | 'submit'
+  autocomplete?: string;
   defaultChildControlConfig?: DynamicControl;
   controls?: DynamicFormConfig['controls'];
   validators?: {
     [key in ValidatorKeys]?: unknown;
+  },
+  asyncValidators?: {
+    [key in AsyncValidatorsKeys]?: unknown;
   },
   suffix?: DynamicControlSuffix;
 }
